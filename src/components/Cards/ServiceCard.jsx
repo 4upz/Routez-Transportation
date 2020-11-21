22680e14258ff8b008576cad2092e1a3702d53e8
Arik Smith <arik@ariks-air.lan>
2020-11-20 22:14:57 -0500

    Add banner orientation and layout

---
+++ src/components/Cards/ServiceCard.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { Box, Image, Flex, Heading, Text, VStack } from "@chakra-ui/react"
+
+const ServiceCard = ({
+  service,
+  description,
+  image,
+  hasColoredBackground,
+  isReversed,
+}) => (
+  <Box bgColor={hasColoredBackground ? "brand.500" : "white"} py={12}>
+    <Flex
+      flexDir={{ base: "column", md: isReversed ? "row-reverse" : "row" }}
+      maxW={1200}
+      mx="auto"
+      p={6}
+      justify="center"
+      align="center"
+    >
+      <Image src={image} w={{ base: "full", md: "50%" }} />
+      <VStack spacing={6} textAlign={{ base: "center", md: "left" }} p={6}>
+        <Heading as="h2" fontSize={32} w="100%">
+          {service}
+        </Heading>
+        <Text fontSize={18} textAlign="left" color="gray.700" w="100%">
+          {description}
+        </Text>
+      </VStack>
+    </Flex>
+  </Box>
+)
+
+export default ServiceCard
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,14 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import ServiceCard from "../components/Cards/ServiceCard"
+import illustration from "../images/art/about.svg"
+import { Heading } from "@chakra-ui/react"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="Services" />
-    <h1>Hi from the service page</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <Heading as="h1" textAlign="center" mt={6}>
+      Services We Offer
+    </Heading>
+    <ServiceCard
+      service="Non-Emergency Medical Transportation"
+      description="Routez offers safe transportation to and from medical appointments."
+      image={illustration}
+    />
+    <ServiceCard
+      service="Employment Transportation"
+      description="Routez is happy to set a schedule for riders in need of regular transportation to their places of employment."
+      image={illustration}
+      hasColoredBackground
+      isReversed
+    />
+    <ServiceCard
+      service="Facility/Care Center Transportation"
+      description="Routez offers daily transportation to all riders traveling to activity centers, care centers, and special events."
+      image={illustration}
+    />
   </Layout>
 )
 
