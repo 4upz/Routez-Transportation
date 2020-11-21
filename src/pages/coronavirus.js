2bbd242699eff7d2d138646b2c99defdf8c40ffd
Arik Smith <arik@ariks-air.lan>
2020-11-20 23:22:46 -0500

    Add empty coronavirus page

---
+++ src/pages/coronavirus.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Coronavirus" />
+  </Layout>
+)
+
+export default ContactPage
