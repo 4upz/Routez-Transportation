a90b734453c943a6a0391c84e804a7eef10d079f
4upz <ams382@case.edu>
2020-12-10 09:55:40 -0500

    Add additional text and CTA to service section

--- src/components/Banners/ServiceArea.jsx
+++ src/components/Banners/ServiceArea.jsx
@@ -1,13 +1,14 @@
 import React from "react"
-import { Box, Heading } from "@chakra-ui/react"
+import { Box, Heading, Text } from "@chakra-ui/react"
 import backgroundImage from "../../images/art/cleveland.svg"
+import { Link } from "gatsby"
 
 const ServiceArea = () => (
   <Box
     as="section"
     textAlign="center"
     minH={350}
-    p={8}
+    py={4}
     bgColor="brand.500"
     bgImage={`url(${backgroundImage})`}
     bgPos="bottom center"
@@ -16,6 +17,15 @@ const ServiceArea = () => (
     <Heading as="h2" maxW={1200} mx="auto">
       Servicing the Northeast Ohio Area
     </Heading>
+    <Text my={1} fontSize={18}>
+      Routez Transportation Inc. strives to be your favorite neighborhood
+      partner for all your transportation needs. <br /> To learn more about the
+      Routez family, give us a call or explore our website and be sure to fill
+      out our "Get in Touch" form found{" "}
+      <Link className="inline-link" to="/contact">
+        here.
+      </Link>
+    </Text>
   </Box>
 )
 
